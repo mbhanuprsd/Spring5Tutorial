824011c6f1177ca997412cd9ee22f8b59e06ce25
Bhanu Prasad <mbhanuprsd@gmail.com>
2017-08-29 17:13:52 +0530

    Added parameter for mail method.

--- src/main/java/com/bhanu/spring5tutorial/controllers/MailController.java
+++ src/main/java/com/bhanu/spring5tutorial/controllers/MailController.java
@@ -3,6 +3,7 @@ package com.bhanu.spring5tutorial.controllers;
 import javax.mail.MessagingException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bhanu.spring5tutorial.mail.MailSender;
@@ -18,14 +19,14 @@ public class MailController {
 	}
 
 	@RequestMapping("/mail")
-	public String mail() throws MessagingException {
+	public String mail(@RequestParam("email") String email) throws MessagingException {
 
-		mailSender.send("mbhanuprsd@gmail.com", "Test Subject", "Body of test mail");
+		mailSender.send(email, "Test Subject", "Body of test mail");
 
 		if (mailSender instanceof SmtpMailSender) {
-			return "Smtp mail Sent!";
+			return "Smtp mail sent to " + email;
 		} else {
-			return "Mock mail sent!";
+			return "Mock mail sent to " + email;
 		}
 	}
 }
