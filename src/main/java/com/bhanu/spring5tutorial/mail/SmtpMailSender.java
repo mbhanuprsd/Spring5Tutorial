54f201534ae5fa62363334f4d9a30cd067111583
Bhanu Prasad Merakanapalli <mbhanuprsd@gmail.com>
2017-08-29 16:41:17 +0530

    Format code

--- src/main/java/com/bhanu/spring5tutorial/mail/SmtpMailSender.java
+++ src/main/java/com/bhanu/spring5tutorial/mail/SmtpMailSender.java
@@ -3,14 +3,11 @@ package com.bhanu.spring5tutorial.mail;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class SmtpMailSender implements MailSender {
-	
-	private static Log log = LogFactory.getLog(SmtpMailSender.class);
+
 	private JavaMailSender javaMailSender;
 
 	public SmtpMailSender(JavaMailSender javaMailSender) {
@@ -19,17 +16,18 @@ public class SmtpMailSender implements MailSender {
 
 	@Override
 	public void send(String to, String subject, String body) throws MessagingException {
-		/*log.info("Sending smtp mail to " + to);
-		log.info("with subject " + subject);
-		log.info("and body " + body);*/
-		
+		/*
+		 * log.info("Sending smtp mail to " + to); log.info("with subject " + subject);
+		 * log.info("and body " + body);
+		 */
+
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, true);
-		
+
 		helper.setTo(to);
 		helper.setSubject(subject);
 		helper.setText(body, true);
-		
+
 		javaMailSender.send(message);
 	}
 
